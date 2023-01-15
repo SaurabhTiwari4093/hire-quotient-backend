9805a4932a598fc23c63d5a39a236d1747b74838
Saurabh Tiwari <86095513+SaurabhTiwari4093@users.noreply.github.com>
2023-01-15 14:56:39 +0530

    Updated pdf reader package

--- routes/pdf.js
+++ routes/pdf.js
@@ -1,32 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const PDFExtract = require('pdf.js-extract').PDFExtract;
+const pdfParse = require('pdf-parse')
 const fs = require('fs');
 
 
 //Get data from pdf
 router.get('/reader', async (req, res) => {
     try {
-        const pdfExtract = new PDFExtract();
         const buffer = fs.readFileSync("./testing/HackathonJDs/JD1.pdf");
-        const options = {};
-        pdfExtract.extractBuffer(buffer, options, (err, data) => {
-            if (err) {
-                return console.log(err);
-            }
-            else {
-                console.log(data);
-                res.status(200).json({
-                    status: 200,
-                    pdfData: data
-                })
-            }
+        pdfParse(buffer).then((data) => {
+            console.log(data);
+            res.status(200).json({
+                status: 200,
+                pdfText: data.text
+            })
         });
     }
-    catch (err) {
+    catch (error) {
+        console.log(error)
         res.status(500).json({
             status: 500,
-            message: err.message
+            message: error.message
         })
     }
 })
